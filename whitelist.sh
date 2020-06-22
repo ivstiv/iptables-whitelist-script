da4f7312e409806b5e6bfd4535fd66204e313a92
Ivan <ivan21039@gmail.com>
2020-06-22 15:51:55 +0100

    The whole script was reworked.. posix compliance, safety breaks, loopback whitelisting etc.

--- whitelist.sh
+++ whitelist.sh
@@ -1,72 +1,61 @@
-#!/bin/bash
-# saner programming env: these switches turn some bugs into errors
-set -o errexit -o pipefail -o noclobber -o nounset -e
-
-err_report() {
-    echo "Error on line $1"
+#!/bin/sh
+checkDependencies() {
+    mainShellPID="$$"
+    printf "curl\niptables\ngrep\ncut" | while IFS= read -r program; do
+        if ! [ -x "$(command -v "$program")" ]; then
+            echo "Error: $program is not installed." >&2
+            kill -9 "$mainShellPID" 
+        fi
+    done
 }
 
-trap 'err_report $LINENO' ERR
+# contains(string, substring)
+# Returns 0 if the specified string contains the specified substring,
+# otherwise returns 1.
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 0    # $substring is in $string
+    else
+        return 1    # $substring is not in $string
+    fi
+}
 
-# allow a command to fail with !’s side effect on errexit
-# use return value from ${PIPESTATUS[0]}, because ! hosed $?
-! getopt --test > /dev/null
-if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo 'I’m sorry, `getopt --test` failed in this environment.'
-    exit 1
-fi
 
-OPTIONS=l:p:ruh
-LONGOPTS=list:,ports:,remove,update,help
-
-# regarding ! and PIPESTATUS see above
-# temporarily store output to be able to check for errors
-# activate quoting/enhanced mode (e.g. by writing out “--options”)
-# pass arguments only via   -- "$@"   to separate them correctly
-! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
-if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    # e.g. return value is 1
-    #  then getopt has complained about wrong arguments to stdout
-    exit 2
-fi
-# read getopt’s output this way to handle the quoting right:
-eval set -- "$PARSED"
+checkDependencies
+###################
+# PROCESS ARGUMENTS #
+###################
 
 remove=n listFile='' ports='' update=n help=n
-while true; do
-    case "$1" in
-        -r|--remove)
-            remove=y
-            shift
-            ;;
-        -l|--list)
-            listFile="$2"
-            shift 2
-            ;;
-        -p|--ports)
-            ports="$2"
-            shift 2
-            ;;
-        -h|--help)
-            help=y
-            shift
-            ;;
-        -u|--update)
-            shift
-            update=y
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Programming error"
-            exit 3
-            ;;
-    esac
+# loop over the arguments
+# leaving this here just in case https://stackoverflow.com/questions/34434157/posix-sh-syntax-for-for-loops-sc2039
+while [ -n "$1" ]; do
+    if [ "$1" = "--remove" ] || [ "$1" = "-r" ]; then 
+        remove=y
+    elif [ "$1" = "--update" ] || [ "$1" = "-u" ]; then
+        update=y
+    elif [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+        help=y
+    elif [ "$1" = "--list" ] || [ "$1" = "-l" ]; then
+        shift
+        listFile="$1"
+    elif [ "$1" = "--ports" ] || [ "$1" = "-p" ]; then
+        shift
+        ports="$1"
+    else 
+        echo "Invalid argument: $1. Run with --help to see available options." && exit
+    fi
+    shift
 done
 
-if [[ $help == 'y' ]]; then
+######################
+# EXECUTE THE COMMANDS #
+######################
+
+if [ $help = 'y' ]; then
 	cat << EOM
 Usage: bash whitelist.sh [options]
 	
@@ -79,62 +68,90 @@ EOM
 	exit 0
 fi
 
-if [[ $update == 'y' ]]; then
-	echo -e "Updating ip lists..."
+if [ $update = 'y' ]; then
+	echo "Updating ip lists..."
 	currentDir=$(pwd)
-	rm $currentDir/stackpath.list $currentDir/cloudflare.list || ! echo "You need to execute the script from its own directory to update them!" || exit 1
-	wget -qO - https://support.stackpath.com/hc/en-us/article_attachments/360041383752/ipblocks.txt > $currentDir/stackpath.list
+    if [ ! -f "$currentDir/cloudflare.list" ] || [ ! -f "$currentDir/stackpath.list" ] ; then
+        echo "[Error]: cloudflare.list or stackpath.list not found!" >&2
+        echo "You need to execute the script from its own directory to update them!" >&2
+        exit 1
+    fi
+	curl -s "https://support.stackpath.com/hc/en-us/article_attachments/360041383752/ipblocks.txt" > "$currentDir/stackpath.list"
 	echo "$currentDir/stackpath.list was updated."
-	wget -qO - https://www.cloudflare.com/{ips-v4,ips-v6} > $currentDir/cloudflare.list
+	curl -s "https://www.cloudflare.com/{ips-v4,ips-v6}" > "$currentDir/cloudflare.list"
 	echo "$currentDir/cloudflare.list was updated."
 fi
 
-if [[ $remove == 'y' ]]; then
+if [ $remove = 'y' ]; then
 	echo "Removing WHITELIST chain from iptables."
-	# remove reference to the chain from prerouting
-	index=$(/sbin/iptables -t mangle -L PREROUTING --line-numbers | grep WHITELIST | awk '{print $1}' || true)
-	[[ -n "$index" ]] && /sbin/iptables -t mangle -D PREROUTING $index || echo "Could not find any references in -t mangle of PREROUTING."
-	/sbin/iptables -t mangle -F WHITELIST || true
-	/sbin/iptables -t mangle -X WHITELIST || true
+    rule=$(/sbin/iptables -t mangle -S PREROUTING -w | grep "WHITELIST" | cut -f 1 -d ' ' --complement)
+    if [ -z "$rule" ]; then
+        echo "Could not find any references in -t mangle of PREROUTING."
+    else
+        eval /sbin/iptables -t mangle -D "$rule" -w
+        echo "Removed: $rule"
+    fi
+	/sbin/iptables -t mangle -F WHITELIST -w
+	/sbin/iptables -t mangle -X WHITELIST -w
 	# do the same for ip6tables
-	index=$(/sbin/ip6tables -t mangle -L PREROUTING --line-numbers | grep WHITELIST | awk '{print $1}' || true)
-	[[ -n "$index" ]] && /sbin/ip6tables -t mangle -D PREROUTING $index || echo "Could not find any references in -t mangle of PREROUTING."
-	/sbin/ip6tables -t mangle -F WHITELIST || true
-	/sbin/ip6tables -t mangle -X WHITELIST || true
-	echo "Successfully removed!"
+    rule=$(/sbin/ip6tables -t mangle -S PREROUTING -w | grep "WHITELIST" | cut -f 1 -d ' ' --complement)
+    if [ -z "$rule" ]; then
+        echo "Could not find any references in -t mangle of PREROUTING."
+    else
+        eval /sbin/ip6tables -t mangle -D "$rule" -w
+        echo "Removed: $rule"
+    fi
+	/sbin/ip6tables -t mangle -F WHITELIST -w
+	/sbin/ip6tables -t mangle -X WHITELIST -w
 fi
 
-[[ -z $listFile ]] && exit 0
-[[ ! -e $listFile ]] && echo "Error: $listFile not found!" && exit 1
+[ -z "$listFile" ] && exit 0
+[ ! -e "$listFile" ] && echo "Error: $listFile file not found!" >&2 && exit 1
 
-echo -e "\nCreating a new chain WHITELIST."
-/sbin/iptables -t mangle -N WHITELIST || exit 1
-/sbin/ip6tables -t mangle -N WHITELIST || exit 1
+if [ -z "$ports" ]; then
+    while true; do
+        echo "You have not specified any ports. THIS COULD POSSIBLY LOCK YOU OUT OF THE SYSTEM!"
+        printf "Do you want to continue? (yes/no):"
+        read -r yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) echo "Stopping..." && exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
+printf "\nCreating a new chain WHITELIST.\n"
+/sbin/iptables -t mangle -N WHITELIST
+/sbin/ip6tables -t mangle -N WHITELIST
+# Whitelisting any traffic from the loopback interface
+/sbin/iptables -t mangle -I WHITELIST -i lo -m comment --comment "Accept everything from loopback" -j ACCEPT
+/sbin/ip6tables -t mangle -I WHITELIST -i lo -m comment --comment "Accept everything from loopback" -j ACCEPT
 
 # add the ip ranges
 while IFS= read -r address
 do
 	# check type of address
-	if [[ $address =~ .*:.* ]]; then
+	if contains "$address" "::"; then
 		echo "Adding IPv6: $address"
-		/sbin/ip6tables -t mangle -A WHITELIST -s $address -j ACCEPT
+		/sbin/ip6tables -t mangle -A WHITELIST -s "$address" -j ACCEPT
 	else
 		echo "Adding IPv4: $address"
-		/sbin/iptables -t mangle -A WHITELIST -s $address -j ACCEPT
+		/sbin/iptables -t mangle -A WHITELIST -s "$address" -j ACCEPT
 	fi
 done < "$listFile"
 /sbin/iptables -t mangle -A WHITELIST -j DROP
 /sbin/ip6tables -t mangle -A WHITELIST -j DROP
 
 
-echo -e "\nCreating a reference to WHITELIST in mangle table of PREROUTING."
+printf "\nCreating a reference to WHITELIST in mangle table of PREROUTING.\n"
 
-if [[ -z $ports ]]; then
+if [ -z "$ports" ]; then
 	/sbin/iptables -t mangle -I PREROUTING -j WHITELIST
 	/sbin/ip6tables -t mangle -I PREROUTING -j WHITELIST
 else
-	/sbin/iptables -t mangle -I PREROUTING -p tcp -m multiport --dports $ports -j WHITELIST
-	/sbin/ip6tables -t mangle -I PREROUTING -p tcp -m multiport --dports $ports -j WHITELIST
+	/sbin/iptables -t mangle -I PREROUTING -p tcp -m multiport --dports "$ports" -j WHITELIST
+	/sbin/ip6tables -t mangle -I PREROUTING -p tcp -m multiport --dports "$ports" -j WHITELIST
 fi
 
 echo "Finished! Check your new iptables rules with iptables -t mangle -L. Run the script with -r/--remove to undo all changes."
